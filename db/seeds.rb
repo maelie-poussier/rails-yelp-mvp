e10520ab736e35682ccc602cca70f1ed76274a25
maelie poussier <maelie.poussier@icloud.com>
2020-08-12 11:35:12 +0200

    add controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: "walk to walk", address: "paris", category: "japanese" )
+restaurant2 = Restaurant.create!(name: "trattoria", address: "pornichet", category: "italian" )
+restaurant3 = Restaurant.create!(name: "mcdo", address: "bordeaux", category: "french" )
+restaurant4 = Restaurant.create!(name: "pizza hut", address: "lyon", category: "belgian" )
+restaurant5 = Restaurant.create!(name: "bourlingueur", address: "grenoble", category: "italian" )
